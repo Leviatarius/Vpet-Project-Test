b09eb0de14471e46da238662e5d67ff4acc04286
lordcad <mrdjmuro@gmail.com>
2019-07-21 00:01:47 -0300

    update

---
+++ Assets/Scenes/ScriptTickTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScriptTickTest : MonoBehaviour
+{ public static float ticktest;
+    public Text ticktesttext;
+    // Start is called before the first frame update
+    void Start()
+    {  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ticktest = ScriptTimer.ticks;
+        ticktesttext.text = " " + ticktest.ToString("f0");
+    }
+}
